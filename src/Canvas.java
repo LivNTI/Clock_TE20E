f8375ad752359cdb4643007c673319cd548dbf19
LivNTI <107177980+LivNTI@users.noreply.github.com>
2023-02-06 10:40:45 +0100

    Clock face done

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class Canvas {
+import javax.swing.*;
+import java.awt.*;
+
+public class Canvas extends JPanel {
+    //attributes
+    private int size;
+    double percent;
+    int dotSize;
+
+    //constuctor
+    public Canvas(int num) {
+        size = num;
+        this.setBackground(Color.CYAN);
+        this.setPreferredSize(new Dimension(size, size));
+    }
+
+    //methods
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        face(g);
+        drawNumbers(g);
+        hours(g);
+    }
+
+    public void face(Graphics g) {
+        g.setColor(Color.white);
+        percent = 0.75;
+        int sizeO = (int) (size * percent);
+        int pos = (int) (size * ((1 - percent) / 2));
+        g.fillOval(pos, pos, sizeO, sizeO);
+
+        dotSize = 5;
+        int cpPos = (size - dotSize) / 2;
+        g.setColor(Color.black);
+        g.fillOval(cpPos, cpPos, dotSize, dotSize);
+    }
+
+    public void drawNumbers(Graphics g) {
+
+
+        int radie = (int) (size * ((percent - 0.1) / 2));
+        double angle = 2 * Math.PI / 12;
+        for (int i = 1; i <= 12; i++) {
+
+            int dx = (int) (radie * Math.sin(angle * i));
+            int dy = (int) (radie * Math.cos(angle * i));
+            System.out.println("dx " + dx);
+            System.out.println("dy " + dy);
+            int temp = (size / 2);
+            int posx = temp + dx;
+            int posy = temp - dy;
+            String num = Integer.toString(i);
+            g.drawString(num, posx, posy);
+            //g.drawOval(posx, posy, dotSize, dotSize);
+        }
+    }
+
+    //Draws the hour hand
+    public void hours(Graphics g) {
+
+    }
+
+    //get & set
 }
+
+
+
+
+
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        start();
+
         System.out.println("Hello world!");
+    }
+
+    public static void start() {
+        int size = 400;
+
+        //create the frame
+        JFrame myFrame = new JFrame(" Hermes");
+        myFrame.setSize(new Dimension(size, size));
+
+        //base functionality
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setResizable(false);
+
+        // add the clock face
+        myFrame.add(new Canvas(size));
+
+        //pack the frame
+        myFrame.pack();
+        myFrame.setVisible(true);
+
     }
 }
